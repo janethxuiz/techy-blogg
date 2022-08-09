e1f246953ea1a3605d5230f1b6c9de2ef2350d4a
janethxuiz <janethruiz152@gmail.com>
2022-08-08 21:18:34 -0700

    added sequelized into connections js file

---
+++ config/connections.js
@@ -0,0 +1,16 @@
+const sequelized = require("sequelized");
+require("dotenv").config();
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PW,
+    {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+
+module.exports = sequelize;
