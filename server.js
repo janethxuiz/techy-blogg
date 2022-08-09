874f47b54776d16adc5fcb3da172cc97d447b7c3
janethxuiz <janethruiz152@gmail.com>
2022-08-08 21:14:12 -0700

    fixed problem in server js file

--- server.js
+++ server.js
@@ -27,6 +27,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers'));
 
 sequelize.sync({force: false}).then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log("Server is running"));
+    app.listen(process.env.PORT, () => console.log("Server is running"));
 });
